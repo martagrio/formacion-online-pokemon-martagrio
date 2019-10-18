c4983c63312d7d2e2d9051b060a384d6145762a5
martagrio <marta.igr90@gmail.com>
2019-10-18 20:52:13 +0200

    Change function to class

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="app">
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="app">
+      </div>
+    );
+  }
 }
 
 export default App;
