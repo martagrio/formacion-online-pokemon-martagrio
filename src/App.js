64b5b314eed9629694a02c6c3bdedadacf3fa10c
martagrio <marta.igr90@gmail.com>
2019-10-19 20:11:58 +0200

    Add favicon

--- src/App.js
+++ src/App.js
@@ -4,9 +4,18 @@ import './App.css';
 class App extends React.Component {
   constructor(props) {
     super(props);
+
+		this.state = {
+			pokemons: {}
+		}
   }
 
+fetchPokemon() {
+	console.log('hola');
+} 
+
   render() {
+		this.fetchPokemon();
     return (
       <div className="app">
       </div>
