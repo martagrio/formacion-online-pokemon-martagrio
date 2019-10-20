e1c9a8bba81f0198774b13d50aa630be7280fa95
martagrio <marta.igr90@gmail.com>
2019-10-20 13:27:22 +0200

    Add structure for pokemon cards

--- src/App.js
+++ src/App.js
@@ -6,25 +6,68 @@ class App extends React.Component {
     super(props);
 
 		this.state = {
-			pokemons: {}
+			pokemons: []
 		}
   }
+
+componentDidMount() {
+	this.fetchPokemon();
+}
 
 fetchPokemon() {
 	const ENDPOINT = 'http://pokeapi.salestock.net/api/v2/';
 	fetch(ENDPOINT)
 	.then(response => response.json())
-	.then(data => fetch(data.pokemon))
+	.then(data => {
+	  const limit = '?limit=';
+	 	const pokeNumber = '25';
+		return(
+			fetch(data.pokemon + limit + pokeNumber)
+		)
+	})
 	.then(pokemons => pokemons.json())
-	.then(pokemons => console.log(pokemons.results))
+	.then(pokemons => {
+		console.log(pokemons.results);
+		this.setState({
+			pokemons: pokemons.results
+		})
+	})
 }
 
   render() {
-		this.fetchPokemon();
+		const {pokemons} = this.state;
     return (
       <div className="app">
-				<label htmlFor="search">Please, introduce the name of a pokemon:</label>
-				<input type="text" name="search" id="search"/>
+				<header className="app__header">
+					<h1 className="header__title">pokedex</h1>
+				</header>
+				<main className="app__main">
+					<label htmlFor="search">Please, introduce the name of a pokemon:</label>
+					<input type="text" name="search" id="search"/>
+					<ol className="pokemons__list">
+						{
+							pokemons.map((pokemon, index) => {
+								return (
+									<li className="pokemon__card" key={index + 1}>
+										<div className="card__container">
+											<img src="" alt={pokemon.name}/>
+											<h2 className="pokemon__name">{pokemon.name}</h2>
+											<p className="pokemon__id">{'ID/' + (index + 1)}</p>
+											<ul className="pokemon_types">
+												<li className="pokemon_type">
+													paco
+												</li>
+											</ul>
+										</div>
+									</li>
+								)
+							})
+						}
+					</ol>
+				</main>
+				<footer className="app__footer">
+
+				</footer>
       </div>
     );
   }
