a26b2868319d59351cd0a657deb7d73599b97e20
martagrio <marta.igr90@gmail.com>
2019-10-28 01:03:44 +0100

    Run docs

---
+++ docs/precache-manifest.7ce1048d21e40599fd845ca42d8b33eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b3ce76da3f564a6cb090cc0e5c5ffba",
+    "url": "./index.html"
+  },
+  {
+    "revision": "69dc1a22ea894d97df40",
+    "url": "./static/css/main.95f4c2b9.chunk.css"
+  },
+  {
+    "revision": "3cf0515f456f89805c2a",
+    "url": "./static/js/2.184ee686.chunk.js"
+  },
+  {
+    "revision": "69dc1a22ea894d97df40",
+    "url": "./static/js/main.fd05cd4c.chunk.js"
+  },
+  {
+    "revision": "da2768afb6b56e228308",
+    "url": "./static/js/runtime-main.a6abfef3.js"
+  }
+]);
